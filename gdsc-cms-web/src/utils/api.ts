69ab504d837ba6478039a5bff01127138e72b7d2
aspiringcoder6 <127761097+aspiringcoder6@users.noreply.github.com>
2025-06-10 20:49:20 +0700

    Small fix

--- gdsc-cms-web/src/utils/api.ts
+++ gdsc-cms-web/src/utils/api.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
-
 export default axios.create({
-    baseURL: `http://localhost:4000/api/`,
+    baseURL: import.meta.env.VITE_API_URL || "http://localhost:4000/api",
 });
